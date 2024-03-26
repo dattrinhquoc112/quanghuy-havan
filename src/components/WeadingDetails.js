f6ae0697b16cd33f9386c4ae1362176779cd6ef1
Dat Trinh Quoc <dattrinhquoc112@gmail.com>
2024-03-27 05:16:26 +0700

    wedding image

--- src/components/WeadingDetails.js
+++ src/components/WeadingDetails.js
@@ -9,10 +9,24 @@ import PHUQUOC_3 from "../images/phuquoc_3.jpg";
 import TET_1 from "../images/tet_1.jpg";
 import TET_2 from "../images/tet_2.jpg";
 import TET_3 from "../images/tet_3.jpg";
+import ANHCUOI_1 from "../images/anhcuoi_1.jpg";
+import ANHCUOI_2 from "../images/anhcuoi_2.jpg";
+import ANHCUOI_3 from "../images/anhcuoi_3.jpg";
+import ANHCUOI_4 from "../images/anhcuoi_4.jpg";
+import ANHCUOI_5 from "../images/anhcuoi_5.jpg";
+import ANHCUOI_6 from "../images/anhcuoi_6.jpg";
+import ANHCUOI_7 from "../images/anhcuoi_7.jpg";
 import { Fade } from "react-reveal";
 
 const WeadingDetails = () => {
   const listImages = [
+    ANHCUOI_1,
+    ANHCUOI_2,
+    ANHCUOI_3,
+    ANHCUOI_4,
+    ANHCUOI_5,
+    ANHCUOI_6,
+    ANHCUOI_7,
     HAGIANG_1,
     HAGIANG_2,
     HOABINH_1,
@@ -43,6 +57,8 @@ const WeadingDetails = () => {
                 src={elem}
                 style={{
                   width: "200px",
+                  height: "fit-content",
+                  alignSelf: "center",
                   objectFit: "contain",
                   borderRadius: "20px",
                 }}
